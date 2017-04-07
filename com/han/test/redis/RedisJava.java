518d14536865f1dabef86d9aa863add73cc992bf
hanfei3 <278413984@qq.com>
2017-04-07 16:10:52 +0800

    redis使用测试

---
+++ com/han/test/redis/RedisJava.java
@@ -0,0 +1,27 @@
+package han.test.redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * test redis by java
+ * Created by hanfei3 on 2017/4/7.
+ */
+public class RedisJava {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("10.87.12.60", 6379);
+        jedis.auth("rootHan");
+        jedis.set("name","testHan");
+        String name = jedis.get("name");
+        System.out.println(name);
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
+//        jedis.lpush("nameList","name1");
+//        jedis.lpush("nameList","name2");
+        List<String> nameList = jedis.lrange("nameList", 0, 2);
+        jedis.expire("nameList",100);
+        System.out.println(nameList);
+    }
+}
